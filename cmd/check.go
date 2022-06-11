2a08cfe6ffcfcd2c5dc8191aad351bcc8347c8a5
Mostafa Farghaly <mostafa@kotal.co>
2022-06-12 02:59:37 +0400

    check that kotal control plane namespace doesn't exist

--- cmd/check.go
+++ cmd/check.go
@@ -7,7 +7,9 @@ import (
 
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
@@ -27,33 +29,38 @@ var checkCmd = &cobra.Command{
 		var err error
 
 		if client, err = CanCreateKubernetesClient(); err != nil {
-			fmt.Printf("❌ can't create Kubernetes client: %s", err)
+			fmt.Printf("❌ can create Kubernetes client: %s", err)
 			return
 		} else {
 			fmt.Println("✔️ can create Kubernetes client")
 		}
 
 		if err = CanQueryKubernetesAPI(client); err != nil {
-			fmt.Printf("❌ can't query Kubernetes API: %s", err)
+			fmt.Printf("❌ can query Kubernetes API: %s", err)
 			return
 		} else {
 			fmt.Println("✔️ can query Kubernetes API")
 		}
 
-		// kotal namespace doesn't exist
-		// Can create Namespaces
-		// Can create ClusterRoles
-		// Can create ClusterRoleBindings
-		// Can create CustomResourceDefinitions
-		// can create ServiceAccounts
-		// Can create Services
-		// Can create Deployments
-		// Can create Secrets
-		// Certificate manager is installed
-		// Can create cert-manager Certificates
-		// Can create cert-manager Issuers
-		// Can create MutatingWebhookConfiguration
-		// Can create ValidatingWebhookConfiguration
+		if err = NamespaceExists(client); err != nil {
+			fmt.Printf("❌ kotal namespace doesn't exists: %s", err)
+		} else {
+			fmt.Println("✔️ kotal namespace doesn't exist")
+		}
+
+		// TODO: Can create Namespaces
+		// TODO: Can create ClusterRoles
+		// TODO: Can create ClusterRoleBindings
+		// TODO: Can create CustomResourceDefinitions
+		// TODO: can create ServiceAccounts
+		// TODO: Can create Services
+		// TODO: Can create Deployments
+		// TODO: Can create Secrets
+		// TODO: Certificate manager is installed
+		// TODO: Can create cert-manager Certificates
+		// TODO: Can create cert-manager Issuers
+		// TODO: Can create MutatingWebhookConfiguration
+		// TODO: Can create ValidatingWebhookConfiguration
 
 	},
 }
@@ -84,6 +91,25 @@ func CanCreateKubernetesClient() (client.Client, error) {
 func CanQueryKubernetesAPI(client client.Client) error {
 	pods := corev1.PodList{}
 	return client.List(context.Background(), &pods)
+}
+
+// NamespaceExists checks if kotal namespace exists
+func NamespaceExists(client client.Client) error {
+	key := types.NamespacedName{
+		Name: "kotal",
+	}
+	ns := corev1.Namespace{}
+
+	err := client.Get(context.Background(), key, &ns)
+	if apierrors.IsNotFound(err) {
+		return nil
+	}
+
+	if !ns.CreationTimestamp.IsZero() {
+		return fmt.Errorf("namespace does exist")
+	}
+
+	return fmt.Errorf("error getting namespace: %w", err)
 }
 
 func init() {
