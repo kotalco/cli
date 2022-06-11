bd9447eb9569586cb49e35f167d92db970149d10
Mostafa Farghaly <mostafa@kotal.co>
2022-06-11 16:48:03 +0400

    check that Kubernetes client can be created

--- cmd/check.go
+++ cmd/check.go
@@ -2,8 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // checkCmd checks cluster compliance
@@ -13,7 +19,12 @@ var checkCmd = &cobra.Command{
 	Long:  "Checks the underlying cluster is suitable for installing Kotal components",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Check underlying cluster compliance")
-		// Can create kubernetes client
+		if _, err := CanCreateKubernetesClient(); err != nil {
+			fmt.Printf("❌ can't create Kubernetes client: %s", err)
+		} else {
+			fmt.Println("✔️ can create Kubernetes client")
+		}
+
 		// Can query Kubernetes API
 		// kotal namespace doesn't exist
 		// Can create Namespaces
@@ -29,7 +40,30 @@ var checkCmd = &cobra.Command{
 		// Can create cert-manager Issuers
 		// Can create MutatingWebhookConfiguration
 		// Can create ValidatingWebhookConfiguration
+
 	},
+}
+
+// CanCreateKubernetesClient checks if we can create Kubernetes client from config
+func CanCreateKubernetesClient() (client.Client, error) {
+	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("error configuring Kubernetes API client: %w", err)
+	}
+
+	scheme := runtime.NewScheme()
+	clientgoscheme.AddToScheme(scheme)
+
+	opts := client.Options{Scheme: scheme}
+
+	client, err := client.New(config, opts)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new Kubernetes client: %w", err)
+	}
+
+	return client, nil
 }
 
 func init() {
